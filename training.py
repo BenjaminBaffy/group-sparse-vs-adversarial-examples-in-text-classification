e69e2d9dd5169091e02dd37b5369d35e53857532
Benjamin Baffy <benjaminbaffy@gmail.com>
2022-06-27 14:45:53 +0200

    refactor and test on adverserial text

--- training.py
+++ training.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import numpy as np
 import torch
 
 from tqdm import tqdm
@@ -7,6 +8,36 @@ from tqdm import tqdm
 import utils.logger as log_module
 import utils.dataset as datasetutil
 from models.trainable_wrapper import TrainableWrapper
+
+from Detector import Detector
+from AttackLoader import AttackLoader
+
+def evaluate(model, data, labels, batch_size=64, loss_fn=torch.nn.CrossEntropyLoss()):
+    num_samples = len(labels)
+    num_batches = int((num_samples // batch_size) + 1)
+    model.train(False)
+    with torch.no_grad():
+        accu_loss = 0.0
+        correct = 0
+        for batch in tqdm(range(num_batches)):
+            lower = batch * batch_size
+            upper = min((batch + 1) * batch_size, num_samples)
+            data_batch = data[lower:upper]
+            if len(data_batch) == 0:
+                continue
+            labels_batch = labels[lower:upper]
+            labels_batch_tensor = torch.LongTensor(labels_batch).to("cuda")
+            out = model(data_batch)
+
+            loss = loss_fn(out, labels_batch_tensor)
+            accu_loss += float(loss.cpu())
+            
+            pred = out.argmax(1)
+            correct += (pred == labels_batch_tensor).sum().cpu().numpy()
+        print(f"{accu_loss} / {num_samples} = {accu_loss / num_samples:0.10f}")
+        print(f"{correct} / {num_samples} = {correct / num_samples:0.10f}")
+    return accu_loss / num_samples, correct / num_samples
+
 
 
 if __name__ == "__main__":
@@ -18,6 +49,14 @@ if __name__ == "__main__":
         epochs=5,
         batch_size=64,
         lr=1e-4,
+        # AttackLoader
+        data_root_dir="attack-log/strong",
+        data_type="standard",
+        scenario="s1",
+        model_type="bert",
+        attack_type="pwws", # "textfooler"
+        include_fae=False,
+        unbalanced=False,
     )
     if not os.path.isdir(args.log_path):
         os.makedirs(args.log_path)
@@ -33,9 +72,6 @@ if __name__ == "__main__":
 
     num_train_samples = len(trainset["label"])
     num_train_batches = int((num_train_samples // args.batch_size) + 1)
-    
-    num_val_samples = len(valset["label"])
-    num_val_batches = int((num_val_samples // args.batch_size) + 1)
 
     for e in range(args.epochs):
         print(f"Epoch {e + 1}")
@@ -55,56 +91,15 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
 
-
-        wrapper.train(False)
-        with torch.no_grad():
-            accu_loss = 0.0
-            correct = 0
-            for batch in tqdm(range(num_val_batches)):
-                lower = batch * args.batch_size
-                upper = min((batch + 1) * args.batch_size, num_val_samples)
-                examples = trainset[text_key][lower:upper]
-                if len(examples) == 0:
-                    continue
-                labels = trainset["label"][lower:upper]
-                labels_tensor = torch.LongTensor(labels).to("cuda")
-                out = wrapper(examples)
-
-                loss = loss_fn(out, labels_tensor)
-                accu_loss += float(loss.cpu())
-                
-                pred = out.argmax(1)
-                correct += (pred == labels_tensor).sum().cpu().numpy()
-            
-            print("Validation:")
-            print(f"{accu_loss} / {num_val_samples} = {accu_loss / num_val_samples:0.10f}")
-            print(f"{correct} / {num_val_samples} = {correct / num_val_samples:0.10f}")
-
-    wrapper.train(False)
-    with torch.no_grad():
-        accu_loss = 0.0
-        correct = 0
-        for batch in tqdm(range(num_val_batches)):
-            lower = batch * args.batch_size
-            upper = min((batch + 1) * args.batch_size, num_val_samples)
-            examples = testset["text"][lower:upper]
-            if len(examples) == 0:
-                continue
-            labels = testset["label"][lower:upper]
-            labels_tensor = torch.LongTensor(labels).to("cuda")
-            out = wrapper(examples)
-
-            loss = loss_fn(out, labels_tensor)
-            accu_loss += float(loss.cpu())
-            
-            pred = out.argmax(1)
-            correct += (pred == labels_tensor).sum().cpu().numpy()
-
-        print("Test result:")
-        print(f"{accu_loss} / {num_val_samples} = {accu_loss / num_val_samples:0.10f}")
-        print(f"{correct} / {num_val_samples} = {correct / num_val_samples:0.10f}")
+        print("Validation:")
+        evaluate(model=wrapper, data=valset[text_key], labels=valset["label"], batch_size=args.batch_size)
 
 
+    print("Test:")
+    evaluate(model=wrapper, data=testset[text_key], labels=testset["label"], batch_size=args.batch_size)
 
-
-
+    loader = AttackLoader(args, logger, data_type=args.data_type)
+    atck_test, dataset = loader.get_attack_from_csv(batch_size=args.batch_size)
+    atck_test = atck_test[~atck_test["perturbed_text"].isna()]
+    print("Attack:")
+    evaluate(model=wrapper, data=atck_test["perturbed_text"].tolist(), labels=atck_test["ground_truth_output"].tolist(), batch_size=args.batch_size)
